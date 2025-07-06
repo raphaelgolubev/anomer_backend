b211309c6674759ba956505254ab214c7da4f247
Raphael Golubev <raphael.golubev@gmail.com>
2025-07-06 20:22:44 +0300

    обновил конфигурацию

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -30,6 +31,8 @@ class SecuritySettings(BaseSettings):
     secret_pem_file: Path = Field(alias='SECURITY_PRIVATE_JWT', default=Path('certs/jwt-private.pem'))
     public_pem_file: Path = Field(alias='SECURITY_PUBLIC_JWT', default=Path('certs/jwt-public.pem'))
     algorithm: str = Field(default='RS256')
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_minutes: int = 30 * 24 * 60  # 30 дней
 
     model_config = ModelConfig(env_prefix='SECURITY_')
 
@@ -48,14 +51,16 @@ class DatabaseSettings(BaseSettings):
     model_config = ModelConfig(env_prefix='DB_')
 
 
+class AppSettings(BaseSettings):
+    prefix: str = "/api"
+    app_version: str = "0.0.1"
+
+
 class Settings:
+    app = AppSettings()
     security = SecuritySettings()
     server = ServerSettings()
     database = DatabaseSettings()
 
 
 settings = Settings()
-
-# Пример использования
-# from config import settings
-# print(settings.database.host)
