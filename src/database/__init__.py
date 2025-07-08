f5b0f168734311e0657fb87222a3cdf9e576428b
Raphael Golubev <raphael.golubev@gmail.com>
2025-07-08 16:14:11 +0300

    created basic class for db managing

---
+++ src/database/__init__.py
@@ -0,0 +1,54 @@
+from typing import AsyncGenerator
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession
+)
+
+from src.config import settings
+
+
+class Database:
+    def __init__(
+        self, 
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        max_overflow: int = 10,
+        pool_size: int = 5
+    ):
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            max_overflow=max_overflow,
+            pool_size=pool_size
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+    async def check_db_connection(self):
+        async with self.engine.connect() as conn:
+            await conn.execute(text("SELECT 1"))
+
+
+database = Database(
+    url=settings.db.async_dsn,
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    max_overflow=settings.db.max_overflow,
+    pool_size=settings.db.pool_size
+)
