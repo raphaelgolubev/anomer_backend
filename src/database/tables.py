2c945ea8364d494470adadfae2e222828172c235
Raphael Golubev <raphael.golubev@gmail.com>
2025-07-10 21:30:55 +0300

    update users table

--- src/database/tables.py
+++ src/database/tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, declared_attr
+from sqlalchemy.orm import Mapped, DeclarativeBase, declared_attr, mapped_column
 
 from src.config import settings
 from src.database.mixins import UuidMixin, TimestampMixin
@@ -8,10 +8,8 @@ from src.utils.case_converter import camel_case_to_snake_case
 
 class Base(DeclarativeBase):
     __abstract__ = True
-    
-    metadata = MetaData(
-        naming_convention=settings.db.naming_conventions
-    )
+
+    metadata = MetaData(naming_convention=settings.db.naming_conventions)
 
     @declared_attr.directive
     def __tablename__(self) -> str:
@@ -19,7 +17,15 @@ class Base(DeclarativeBase):
 
 
 class User(Base, UuidMixin, TimestampMixin):
-    """ Таблица пользователей """
+    """Таблица пользователей"""
 
     username: Mapped[str] = mapped_column(nullable=True, unique=True)
     """ Имя пользователя """
+
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    """ Электронная почта """
+
+    password: Mapped[str] = mapped_column(nullable=False)
+    """Хэшированный пароль"""
+
+    role: Mapped[str] = mapped_column(nullable=False, default="USER")
