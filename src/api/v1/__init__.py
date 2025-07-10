85c975388dcc584946ec2fec0d5a1314310e55c1
Raphael Golubev <raphael.golubev@gmail.com>
2025-07-10 21:30:16 +0300

    update main_router prefix

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -4,6 +4,6 @@ from src.config import settings
 from src.api.v1.auth.views import router as auth_router
 from src.api.v1.users.views import router as users_router
 
-main_router = APIRouter(prefix=f"{settings.app.prefix}/v1")
+main_router = APIRouter(prefix=f"{settings.app.prefix}{settings.app.v1.prefix}")
 main_router.include_router(auth_router)
 main_router.include_router(users_router)
