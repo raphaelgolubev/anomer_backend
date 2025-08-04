f229ea7c68e6355795ed7a575c7c78c259995295
Raphael Golubev <raphael.golubev@gmail.com>
2025-08-04 22:40:10 +0300

    improve auth flow

---
+++ src/api/v1/auth/service.py
@@ -0,0 +1,108 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jwt.exceptions import InvalidTokenError
+
+from src.security import tokens, hashing_encoding
+from src.database import database
+from src.database.crud import users
+from src.database.tables import User
+
+
+http_bearer = HTTPBearer()
+
+
+async def get_payload_from_token(
+    http_creds: HTTPAuthorizationCredentials = Depends(http_bearer),
+) -> dict:
+    """
+    Получает текущего авторизованного пользователя из токена в заголовке `Authorization`.
+
+    Args:
+        - `http_creds`: заголовок Authorization в формате `Bearer <token>`
+
+    Raises:
+        - `HTTPException`: если токен недействителен.
+
+    Returns:
+        `dict`: payload токена.
+    """
+
+    # получаем токен из заголовка Authorization
+    token = http_creds.credentials
+
+    try:
+        # pyJWT самостоятельно проверит время жизни токена
+        # и выбросит исключение, если токен просрочен
+        payload: dict = hashing_encoding.decode_jwt(
+            token=token
+        )
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = f"Ошибка декодирования токена: {e}"
+        )
+
+    return payload
+
+
+async def get_user_from_payload(payload: dict) -> User | None:
+    """
+    Получает пользователя из БД по email из payload токена.
+    Проверяет, что пользователь активирован.
+
+    Args:
+        - `payload`: payload токена.
+
+    Raises:
+        - `HTTPException`: если пользователь не найден или не активирован.
+
+    Returns:
+        `User`: текущий авторизованный пользователь.
+    """
+
+    # получаем email из payload
+    subject: str | None = payload.get("sub")
+
+    async with database.session_factory() as session:
+        # получаем пользователя из БД
+        if not (user := await users.get_user(session=session, email=subject)):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Пользователь не найден"
+            )
+        # проверяем, что пользователь активирован
+        if not user.is_email_verified:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Пользователь не активен"
+            )
+
+    return user
+
+
+async def get_current_auth_user_for_access(payload: dict = Depends(get_payload_from_token)) -> User | None:
+    """
+    Получает текущего авторизованного пользователя из токена в заголовке `Authorization`.
+    Проверяет, что токен имеет тип "access".
+    """
+    # проверяем, что токен имеет тип "access"
+    if payload.get(tokens.TOKEN_TYPE_FIELD) != tokens.TokenType.ACCESS_TOKEN_TYPE.value:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный тип токена"
+        )
+    return await get_user_from_payload(payload)
+
+
+async def get_current_auth_user_for_refresh(payload: dict = Depends(get_payload_from_token)) -> User | None:
+    """
+    Получает текущего авторизованного пользователя из токена в заголовке `Authorization`.
+    Проверяет, что токен имеет тип "refresh".
+    """
+    # проверяем, что токен имеет тип "refresh"
+    if payload.get(tokens.TOKEN_TYPE_FIELD) != tokens.TokenType.REFRESH_TOKEN_TYPE.value:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный тип токена"
+        )
+    return await get_user_from_payload(payload)
--- src/api/v1/auth/views.py
+++ src/api/v1/auth/views.py
@@ -1,32 +1,35 @@
 from typing import Annotated
 
 from fastapi import Form, Depends, APIRouter, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import src.security.tokens as tokens
-import src.api.v1.auth.validations as validator
+import src.api.v1.auth.service as service
 import src.security.hashing_encoding as jwt_utils
 
-from src.database.ram_db import user_db
+from src.database.crud import users
+from src.database import database
+from src.database.tables import User
 from src.schemas.auth import TokenInfo
-from src.schemas.users import UserCredentials
 
 router = APIRouter(tags=["Авторизация"])
 
 
-def get_login_credentials(
+async def get_login_credentials(
+    session: Annotated[AsyncSession, Depends(database.session_getter)],
     username: str = Form(), 
     password: str = Form()
-):
+) -> User:
     unauthorized_exc = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверные учетные данные"
     )
-    if not (user := user_db.get(username)):
+    if not (user := await users.get_user(session=session, email=username)):
         raise unauthorized_exc
 
     if not jwt_utils.verify_password(password=password, hashed_password=user.password):
         raise unauthorized_exc
 
-    if not user.is_active:
+    if not user.is_email_verified:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Пользователь не активен"
         )
@@ -36,20 +39,58 @@ def get_login_credentials(
 
 @router.post("/login/", response_model=TokenInfo)
 async def login(
-    credentials: Annotated[UserCredentials, Depends(get_login_credentials)],
+    user: Annotated[User, Depends(get_login_credentials)],
 ):
-    access_token = tokens.create_access_token(user=credentials)
-    refresh_token = tokens.create_refresh_token(user=credentials)
+    access_token = tokens.create_token(user=user, token_type=tokens.TokenType.ACCESS_TOKEN_TYPE)
+    refresh_token = tokens.create_token(user=user, token_type=tokens.TokenType.REFRESH_TOKEN_TYPE)
 
     return TokenInfo(access_token=access_token, refresh_token=refresh_token)
 
 
-@router.post("/refresh/", response_model=TokenInfo, response_model_exclude_none=True)
+@router.post("/test_access_token/")
+async def test_access_token(
+    user: Annotated[User | None, Depends(service.get_current_auth_user_for_access)],
+):
+    if user:
+        return {"message": "Token is valid", "user": user.email}
+    return {"message": "Token is invalid"}
+
+
+@router.post("/test_refresh_token/")
+async def test_refresh_token(
+    user: Annotated[User | None, Depends(service.get_current_auth_user_for_refresh)],
+):
+    if user:
+        return {"message": "Token is valid", "user": user.email}
+    return {"message": "Token is invalid"}
+
+
+@router.post("/refresh/", response_model=TokenInfo)
 async def refresh_token(
-    credentials: Annotated[
-        UserCredentials, Depends(validator.get_current_auth_user_for_refresh)
+    user: Annotated[
+        User | None, Depends(service.get_current_auth_user_for_refresh)
     ],
 ):
-    access_token = tokens.create_access_token(user=credentials)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный тип токена"
+        )
 
-    return TokenInfo(access_token=access_token)
+    access_token = tokens.create_token(user=user, token_type=tokens.TokenType.ACCESS_TOKEN_TYPE)
+    refresh_token = tokens.create_token(user=user, token_type=tokens.TokenType.REFRESH_TOKEN_TYPE)
+
+    return TokenInfo(access_token=access_token, refresh_token=refresh_token)
+
+
+@router.post("/logout/")
+async def logout(
+    user: Annotated[User | None, Depends(service.get_current_auth_user_for_access)],
+):
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный тип токена"
+        )
+
+    return {"message": f"Logout placeholder for {user.email}"}
