2c653ccb492ad4e400b078be9cd532736d2f6a6d
Raphael Golubev <raphael.golubev@gmail.com>
2025-08-04 22:42:25 +0300

    removed UserCredentials scheme

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -4,15 +4,6 @@ from uuid import UUID
 from pydantic import Field, EmailStr, BaseModel, ConfigDict
 
 
-class UserCredentials(BaseModel):
-    username: Annotated[str, Field(min_length=3, max_length=20)]
-    password: bytes
-    email: EmailStr | None = None
-    is_active: bool = True
-
-    model_config = ConfigDict(strict=True)
-
-
 class UserCreateIn(BaseModel):
     email: EmailStr
     password: str
