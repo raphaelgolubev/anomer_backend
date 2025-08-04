53f70a15d98babd86a7aa0230b5570b2d84822d0
Raphael Golubev <raphael.golubev@gmail.com>
2025-08-04 21:11:24 +0300

    refactor security modules

--- src/security/hashing_encoding.py
+++ src/security/hashing_encoding.py
@@ -13,6 +13,19 @@ def encode_jwt(
     expire_timedelta: timedelta | None = None,
     expire_minutes: int | None = None,
 ):
+    """
+    Кодирует payload в JWT токен
+
+    Args:
+        - `payload`: данные токена
+        - `private_key`: приватный ключ
+        - `algorithm`: алгоритм шифрования
+        - `expire_timedelta`: время жизни токена
+        - `expire_minutes`: время жизни токена в минутах
+
+    Returns:
+        `str`: JWT токен
+    """
     to_encode = payload.copy()
     now = datetime.now(timezone.utc)
 
@@ -35,19 +48,50 @@ def decode_jwt(
     token: str | bytes,
     public_key: str = settings.security.public_pem_file.read_text(),
     algorithm: str = settings.security.algorithm,
-):
+) -> dict:
+    """
+    Декодирует JWT токен
+
+    Args:
+        - `token`: JWT токен
+        - `public_key`: публичный ключ
+        - `algorithm`: алгоритм шифрования
+
+    Returns:
+        `dict`: данные токена (payload)
+    """
     decoded = jwt.decode(token, public_key, algorithms=[algorithm])
     return decoded
 
 
 def hash_password(password: str) -> bytes:
+    """
+    Хеширует пароль
+
+    Args:
+        - `password`: пароль
+
+    Returns:
+        `bytes`: хешированный пароль
+    """
     salt = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode("utf-8")
 
     return bcrypt.hashpw(pwd_bytes, salt)
 
 
-def verify_password(password: str, hashed_password: bytes) -> bool:
+def verify_password(password: str, hashed_password: str) -> bool:
+    """
+    Проверяет, совпадает ли пароль с хешированным паролем
+
+    Args:
+        - `password`: пароль
+        - `hashed_password`: хешированный пароль
+
+    Returns:
+        `bool`: True, если пароль совпадает с хешированным паролем, False в противном случае
+    """
     return bcrypt.checkpw(
-        password=password.encode("utf-8"), hashed_password=hashed_password
+        password=password.encode("utf-8"), 
+        hashed_password=hashed_password.encode("utf-8")
     )
--- src/security/tokens.py
+++ src/security/tokens.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 import src.security.hashing_encoding as jwt_utils
 from src.config import settings
-from src.schemas.users import UserCredentials
+from src.database.tables import User
 
 TOKEN_TYPE_FIELD = "type"
 
@@ -13,6 +13,25 @@ class TokenType(Enum):
 
 
 def create_jwt(token_type: TokenType, token_data: dict) -> str:
+    """
+    Создает JWT токен и добавляет в него информацию о типе токена
+    в поле "type".
+    
+    Пример токена:
+    ```json
+    {
+        "type": <token_type>,
+        <token_data>,
+    }
+    ```
+
+    Args:
+        - `token_type`: тип токена
+        - `token_data`: данные токена
+
+    Returns:
+        `str`: JWT токен
+    """
     jwt_payload = {TOKEN_TYPE_FIELD: token_type.value}
     jwt_payload.update(token_data)
 
@@ -33,11 +52,27 @@ def create_jwt(token_type: TokenType, token_data: dict) -> str:
             raise ValueError(f"Unknown token type: {token_type}")
 
 
-def create_access_token(user: UserCredentials) -> str:
-    jwt_payload = {"sub": user.username, "email": user.email}
-    return create_jwt(token_type=TokenType.ACCESS_TOKEN_TYPE, token_data=jwt_payload)
+def create_token(user: User, token_type: TokenType) -> str:
+    """
+    Создает JWT токен и добавляет в него информацию о пользователе в
+    поле "sub", информацию о типе токена в поле "type".
 
+    Пример токена:
+    ```json
+    {
+        "sub": "user@example.com",
+        "type": "access"
+    }
+    ```
 
-def create_refresh_token(user: UserCredentials) -> str:
-    jwt_payload = {"sub": user.username}
-    return create_jwt(token_type=TokenType.REFRESH_TOKEN_TYPE, token_data=jwt_payload)
+    Args:
+        - `user`: пользователь
+        - `token_type`: тип токена
+
+    Returns:
+        `str`: JWT токен
+    """
+    jwt_payload = {
+        "sub": user.email,
+    }
+    return create_jwt(token_type=token_type, token_data=jwt_payload)
